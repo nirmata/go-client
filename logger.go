ca9a4c24128b2a2e13b2d33b0e4d88d30fe37b9d
Jim Bugwadia <jim@nirmata.com>
2024-12-30 22:48:16 -0800

    add log level API
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>

--- logger.go
+++ logger.go
@@ -5,4 +5,17 @@ import (
 	"os"
 )
 
-var logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+var (
+	leveler = new(slog.LevelVar)
+	logger  = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: leveler,
+	}))
+)
+
+func init() {
+	LogLevel(slog.LevelWarn)
+}
+
+func LogLevel(l slog.Level) {
+	leveler.Set(l)
+}
